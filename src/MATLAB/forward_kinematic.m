660bda2310a4c3ec3dd60e2c31130e72bd23acc3
yezehao <sgzye5@liverpool.ac.uk>
2023-12-07 16:35:22 +0000

    udpate the matlab code

--- src/MATLAB/forward_kinematic.m
+++ src/MATLAB/forward_kinematic.m
@@ -1,4 +1,4 @@
-function C = forward_kinematic(parameter)
+function [C1,C2] = forward_kinematic(parameter)
     Sr= parameter.Sr; 
     r = parameter.r; 
     d = parameter.d; 
@@ -8,13 +8,24 @@ function C = forward_kinematic(parameter)
     r1= parameter.r1;
     S = n*d;
     R1 = Sr*r1/(Sr-S);
+    % Maximum angle of manipulator
     bend_ang_max=Sr/R1;
     alpha1 = bend_ang_max*(-1+2*rand);
     alpha2 = bend_ang_max*(-1+2*rand);
     deltaS1=-r*alpha1; 
     deltaS3=-r*alpha2;
-    x=-Sr*r/deltaS1+(Sr*r/deltaS1)*cos(deltaS1/r)-d1*sin(deltaS1/r)-Sr*r/deltaS3*sin(deltaS1/r)*sin(deltaS3/r)-d2*sin(deltaS1/r)*cos(deltaS3/r);
-    y=-Sr*r/deltaS3+(Sr*r/deltaS3)*cos(deltaS3/r)-d2*sin(deltaS3/r);
-    z=(Sr*r/deltaS1)*sin(deltaS1/r)+d1*cos(deltaS1/r)+(Sr*r/deltaS3)*sin(deltaS3/r)*cos(deltaS1/r)+d2*cos(deltaS1/r)*cos(deltaS3/r);
-    C = [x y z];
+    x1=-Sr*r/deltaS1+(Sr*r/deltaS1)*cos(deltaS1/r)-d1*sin(deltaS1/r)-Sr*r/deltaS3*sin(deltaS1/r)*sin(deltaS3/r)-d2*sin(deltaS1/r)*cos(deltaS3/r);
+    y1=-Sr*r/deltaS3+(Sr*r/deltaS3)*cos(deltaS3/r)-d2*sin(deltaS3/r);
+    z1=(Sr*r/deltaS1)*sin(deltaS1/r)+d1*cos(deltaS1/r)+(Sr*r/deltaS3)*sin(deltaS3/r)*cos(deltaS1/r)+d2*cos(deltaS1/r)*cos(deltaS3/r);
+    C1 = [x1 y1 z1];
+    % Working angle of manipulator
+    work_ang = pi*60/180;
+    alpha1 = work_ang*(-1+2*rand);
+    alpha2 = work_ang*(-1+2*rand);
+    deltaS1=-r*alpha1; 
+    deltaS3=-r*alpha2;
+    x2=-Sr*r/deltaS1+(Sr*r/deltaS1)*cos(deltaS1/r)-d1*sin(deltaS1/r)-Sr*r/deltaS3*sin(deltaS1/r)*sin(deltaS3/r)-d2*sin(deltaS1/r)*cos(deltaS3/r);
+    y2=-Sr*r/deltaS3+(Sr*r/deltaS3)*cos(deltaS3/r)-d2*sin(deltaS3/r);
+    z2=(Sr*r/deltaS1)*sin(deltaS1/r)+d1*cos(deltaS1/r)+(Sr*r/deltaS3)*sin(deltaS3/r)*cos(deltaS1/r)+d2*cos(deltaS1/r)*cos(deltaS3/r);
+    C2 = [x2 y2 z2];
 end
--- src/MATLAB/workspace_simulation.m
+++ src/MATLAB/workspace_simulation.m
@@ -16,36 +16,65 @@ clear;clc;
 length_mm=300;
 %% Error Calculatoin
 [E,parameter] = error_calculation(length_mm);
-% Plot Figures
-figure;
-subplot(1,2,1);
-for i = 1:1:9
-    plot(E(i).Ea(:,2),E(i).Ea(:,1),'-');
-    hold on;
-end
-xlabel('Number of connectors');
-ylabel('Absolute error(mm)');
-grid on;
-subplot(1,2,2);
-for i = 1:1:9
-    plot(E(i).Er(:,2),E(i).Er(:,1),'-');
-    hold on;
-end
-xlabel('Number of connectors');
-ylabel('Relative error(%)');
-grid on;
+% % Plot Figures
+% figure;
+% subplot(1,2,1);
+% for i = 1:1:9
+%     plot(E(i).Ea(:,2),E(i).Ea(:,1),'-');
+%     hold on;
+% end
+% xlabel('Number of connectors');
+% ylabel('Absolute error(mm)');
+% grid on;
+% subplot(1,2,2);
+% for i = 1:1:9
+%     plot(E(i).Er(:,2),E(i).Er(:,1),'-');
+%     hold on;
+% end
+% xlabel('Number of connectors');
+% ylabel('Relative error(%)');
+% grid on;
 %% Workspace Simulation
-parameter.n = 15; % Number of connectors
+parameter.n = 10; % Number of connectors
 parameter.r1 = 0.21*length_mm;
 parameter.d1 = 0.03*length_mm;
 parameter.d2 = 0.03*length_mm;
-for i = 1:5000
-    C = forward_kinematic(parameter);
-    coordinate(i,:) = C;
+for i = 1:2000
+    [C1,C2] = forward_kinematic(parameter);
+    coord1(i,:) = C1;
+    coord2(i,:) = C2;
 end
+% figure; %%% The subplots of figure %%%
+% s = 0.002*length(coord1(:,1));
+% subplot(1,2,1); % subplot1
+% scatter3(coord1(:,1),coord1(:,2),coord1(:,3),s,"filled",'MarkerEdgeColor',[.93 .69 .13], 'MarkerFaceColor',[1 .84 .28]);
+% grid on;
+% subplot(1,2,2); % subplot3
+% scatter3(coord2(:,1),coord2(:,2),coord2(:,3),s,"filled",'MarkerEdgeColor',[.85 .33 .10], 'MarkerFaceColor',[1 .48 .25]);
+% grid on;
+% hold on;
+
+% figure;
+% s = 0.002*length(coord1(:,1));grid on;
+% scatter3(coord2(:,1),coord2(:,2),coord2(:,3),s,"filled", ...
+%     'MarkerEdgeColor',[.85 .33 .10], ...
+%     'MarkerFaceColor',[1 .48 .25]);
+% hold on;
+% scatter3(coord1(:,1),coord1(:,2),coord1(:,3),s,"filled", ...
+%     'MarkerEdgeColor',[.93 .69 .13], ...
+%     'MarkerFaceColor',[1 .84 .28]);
+% hold on;
+
 figure;
-for i=1:5000
-    plot3(coordinate(i,1),coordinate(i,2),coordinate(i,3),'bo');
-    grid on;
-    hold on;
-end
+s = 0.002*length(coord1(:,1));grid on;
+subplot(1,2,1)
+scatter(coord2(:,1),coord2(:,2),s,"filled", ...
+    'MarkerEdgeColor',[.85 .33 .10], ...
+    'MarkerFaceColor',[1 .48 .25]);
+grid on;
+subplot(1,2,2)
+scatter(coord2(:,1),coord2(:,3),s,"filled", ...
+    'MarkerEdgeColor',[.85 .33 .10], ...
+    'MarkerFaceColor',[1 .48 .25]);
+grid on;
+hold on;
