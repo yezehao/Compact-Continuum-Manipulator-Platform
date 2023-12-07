dc72a859d6d71df01ded33a2b0ea28d829283fe0
yezehao <sgzye5@liverpool.ac.uk>
2023-12-07 15:09:24 +0000

    add functions about workspace simulation programme

---
+++ src/forward_kinematic.m
@@ -0,0 +1,20 @@
+function C = forward_kinematic(parameter)
+    Sr= parameter.Sr; 
+    r = parameter.r; 
+    d = parameter.d; 
+    d1= parameter.d1;
+    d2= parameter.d2;
+    n = parameter.n;
+    r1= parameter.r1;
+    S = n*d;
+    R1 = Sr*r1/(Sr-S);
+    bend_ang_max=Sr/R1;
+    alpha1 = bend_ang_max*(-1+2*rand);
+    alpha2 = bend_ang_max*(-1+2*rand);
+    deltaS1=-r*alpha1; 
+    deltaS3=-r*alpha2;
+    x=-Sr*r/deltaS1+(Sr*r/deltaS1)*cos(deltaS1/r)-d1*sin(deltaS1/r)-Sr*r/deltaS3*sin(deltaS1/r)*sin(deltaS3/r)-d2*sin(deltaS1/r)*cos(deltaS3/r);
+    y=-Sr*r/deltaS3+(Sr*r/deltaS3)*cos(deltaS3/r)-d2*sin(deltaS3/r);
+    z=(Sr*r/deltaS1)*sin(deltaS1/r)+d1*cos(deltaS1/r)+(Sr*r/deltaS3)*sin(deltaS3/r)*cos(deltaS1/r)+d2*cos(deltaS1/r)*cos(deltaS3/r);
+    C = [x y z];
+end
