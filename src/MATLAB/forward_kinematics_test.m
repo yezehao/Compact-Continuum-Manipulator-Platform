3d678f5a4e8252b066de88c82be339a7590b9f18
yezehao <sgzye5@liverpool.ac.uk>
2023-12-15 10:13:26 +0000

    udpate the matlab programme

--- src/MATLAB/forward_kinematics_test.m
+++ src/MATLAB/forward_kinematics_test.m
@@ -3,7 +3,8 @@ clc;
 length_mm = 200;
 Sr=0.5*length_mm; 
 d=0.015*length_mm; 
-angle = 10*[1; 1; 1; 1];
+d=0;
+angle = 90*[1; 1; 0; 0];
 rad = deg2rad(angle);
 for i = 1:4
     % Define the bending angle alpha
@@ -17,29 +18,32 @@ for i = 1:4
         R = Sr/Alpha;
         Dhorz = sign(alpha)*(R*(1-cos(Alpha)) + d*sin(Alpha));
         Dvert = (R*sin(Alpha) + d*cos(Alpha));
+        % disp(Dhorz);disp(Dvert);
     end
 
     % Matric structure initialization
     M(1).bend = eye(3); % the orientation of origin
-    M(1).position = [0;0;0]; % the position of origin
+    M(1).displacement = [0;0;0]; % the position of origin
     if mod(i, 2) == 1
         % Relative Bending Matrices Calculation 
         Mt = [1 0 0;
-             0 cos(alpha) sin(alpha);
-             0 -sin(alpha) cos(alpha)];
-        M(i+1).bend = Mt;
+              0 cos(alpha) sin(alpha);
+              0 -sin(alpha) cos(alpha)];
+        M(i+1).bend = round(Mt*10^5)/10^5; % round five decimal places 
         % Relative Position Matrices Calculation
-        M(i+1).position = [0;Dhorz;Dvert];
+        M(i+1).displacement = [0;Dhorz;Dvert];
     else
         % Relative Bending Matrices Calculation 
         Mt = [cos(alpha) 0 sin(alpha);
-        0 1 0 
-        -sin(alpha) 0 cos(alpha);];
-        M(i+1).bend = Mt;
+              0 1 0 
+              -sin(alpha) 0 cos(alpha)];
+        M(i+1).bend = round(Mt*10^5)/10^5; % round five decimal places 
         % Relative Position Matrices Calculation
-        M(i+1).position = [Dhorz;0;Dvert];
+        M(i+1).displacement = [Dhorz;0;Dvert];
     end
 end
+
+clearvars Mt i Dhorz Dvert Alpha
 
 % There are four units in the manipulator, the base coordinate and end 
 % effector coordinate are stored in the matrix "coordinate"
@@ -49,13 +53,13 @@ M(2).B = M(1).bend*M(2).bend;
 M(3).B = M(1).bend*M(2).bend*M(3).bend;
 M(4).B = M(1).bend*M(2).bend*M(3).bend*M(4).bend;
 M(5).B = M(1).bend*M(2).bend*M(3).bend*M(4).bend*M(5).bend;
-position(:,1) = M(1).position; % The origin
+position(:,1) = M(1).displacement; % The origin
 node(1).position = position(:,1);
 node(1).coordinate = M(1).bend;
 for i = 2:5
-    position(:,i) = M(i-1).B*M(i).position + position(:,i-1);
+    position(:,i) = M(i-1).B*M(i).displacement + position(:,i-1);
     node(i).position = position(:,i);
-    node(i).coordinate = 50*M(i).bend;
+    node(i).coordinate = 50*M(i).B;
 end
 clearvars alpha coord_origin i rad symbol
 
@@ -76,7 +80,7 @@ for i = 1:4
     hold on;
 end
 
-% Plot the coordinate
+% Plot the sub-coordinate
 for i = 2:5
 quiver3(position(1,i),position(2,i),position(3,i), ...
         node(i).coordinate(1,1),node(i).coordinate(1,2),node(i).coordinate(1,3), ...
