2164b40a64505a2f6259a64b048543447b12a3d9
yezehao <sgzye5@liverpool.ac.uk>
2024-01-04 06:59:20 +0000

    update the programme

--- src/MATLAB/workspace_simulation.m
+++ src/MATLAB/workspace_simulation.m
@@ -1,4 +1,4 @@
-clear;clc;
+clear;clc;close all;tic;
 %% Parameter Define
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Sr: the arc length of the bending curve                                 %
@@ -19,12 +19,10 @@ Nu= 4; % The number of units
 N = [10;10;10;10]; % Number of connectors
 r = [50;50;40;40];
 d = 0.03*Sr*ones(Nu,1);
-angle = [10;10;10;10]; % The bending angle
 for i = 1:Nu
     parameter(i).n = N(i,1);
     parameter(i).d = d(i,1);
     parameter(i).r = r(i,1);
-    parameter(i).rad = deg2rad(angle(i,1));
     S = parameter(i).n*parameter(i).d;
     parameter(i).R = Sr*parameter(i).r/(Sr-S);
     parameter(i).bend_rad_max = Sr/parameter(i).R; % maximum rad of manipulator
@@ -32,7 +30,8 @@ for i = 1:Nu
 end
 clearvars S i d r
 
-for i = 1:50000
+index = 100000;
+for i = 1:index
     for j = 1:Nu
         parameter(j).rad = parameter(j).bend_rad_max*(-1+2*rand);
     end
@@ -45,15 +44,18 @@ for i = 1:50000
     % node = FK_matrix(parameter,Sr);
     % position(:,i) = node(Nu+1).position;
     % coordinate(:,3*i-2:3*i)= node(Nu+1).coordinate;
+    if mod(i, index/100) == 0
+        disp(['Iteration: ', num2str(i)]);
+    end
 end
+clearvars i j
 
 % %% Result Save
 % writematrix(position, 'result/maximum_bending_workspace.csv');
 
 %% Result Display
-figure;
-s = 5;grid on,
-% Plot the coordinate
+% Plot Entire Workspace
+figure;s = 5;grid on,
 quiver3(0, 0, 0, 500, 0, 0, 'r', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
 quiver3(0, 0, 0, 0, 500, 0, 'g', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
 quiver3(0, 0, 0, 0, 0, 500, 'b', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
@@ -64,3 +66,42 @@ title('The workspace with work angle of ± 90 degree');
 xlabel('X');
 ylabel('Y');
 zlabel('Z');
+% Plot Useful Workspace
+H = 260;
+range = [-150,150;-150,150;H,H+300];
+position_u = position(:, ...
+    position(1, :) >= range(1,1) & ...
+    position(1, :) <= range(1,2) & ...
+    position(2, :) >= range(2,1) & ...
+    position(2, :) <= range(2,2) & ...
+    position(3, :) >= range(3,1) & ...
+    position(3, :) <= range(3,2));
+figure;s = 5;grid on,
+quiver3(0, 0, 0, 500, 0, 0, 'r', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
+quiver3(0, 0, 0, 0, 500, 0, 'g', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
+quiver3(0, 0, 0, 0, 0, 500, 'b', 'LineWidth', 2, 'MaxHeadSize', 0.1); hold on,
+scatter3(position_u(1,:),position_u(2,:),position_u(3,:),s,"filled", ...
+    'MarkerEdgeColor',[.85 .33 .10], ...
+    'MarkerFaceColor',[1 .48 .25]);
+title('The workspace with work angle of ± 90 degree');
+xlabel('X');
+ylabel('Y');
+zlabel('Z');
+% Key Points Inspection
+KP_vertical = [range(3,1),range(3,1)+100,range(3,1)+200,range(3,1)+300];
+element = 150*[-1,0,1];
+KP_horizontal = [repelem(element,1,3);repmat(element,1,3)];
+KP = [repmat(KP_horizontal,1,4);repelem(KP_vertical,1,9)];
+distance = pdist2(position_u', KP');
+[distance_min, ~] = min(distance);
+disp('Distance to the nearest point:');
+for i = 1:4
+    disp(['Height:',num2str(range(3,1)+100*(i-1)),' mm'])
+    disp(distance_min(1,9*i-8:9*i))
+end
+clearvars H element distance KP_vertical KP_horizontal i s
+
+%% Execution Time
+T = toc;
+disp(['The execution time of the programme is', num2str(T), 's']);
+disp(['The simulation points per second of the programme is ',num2str(index/T)]);
