d3128dbf0ad7cdcce512182e64a2c33393ed3008
yezehao <sgzye5@liverpool.ac.uk>
2024-03-03 18:12:06 +0000

    update the FABRIKc functions

--- Kinematics/FABRIKc.py
+++ Kinematics/FABRIKc.py
@@ -1,22 +1,16 @@
 # Module Import
 import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
+from prettytable import PrettyTable
 
-Sr = 150 # unit: mm
-# Virtual Node Initial Define
-virtual_node = np.zeros((4,3))
-# Node Initial Define
-node = np.zeros((4,3))
-# Virtual Link Length Define
-l = np.array([0, 0, 0, 0])
 
-def forward_reaching_phase(target, orientation, cita, l, virtual_node, node):
+def forward_reach(target, orientation, cita, l, virtual_node, node, Sr):
     # Iteration 1
     virtual_node[3,:] = target - l[3]*orientation[2,:]
     virtual = virtual_node[3,:] - virtual_node[2,:]
     vector = orientation @ virtual
     cita[3] = -np.arctan2(vector[0],vector[2])
+    # if (cita[3] > np.pi/2) or (cita[3] < -np.pi/2):
+    #     cita[3] = 0.5*cita[3]  
     # vector = (virtual) + (virtual) * orientation[1,:]
     # cita[3] = np.arccos(vector @ orientation[2,:]/(np.linalg.norm(vector)*np.linalg.norm(orientation[2,:])))
     if cita[3]==0:
@@ -35,6 +29,8 @@ def forward_reaching_phase(target, orientation, cita, l, virtual_node, node):
     virtual = virtual_node[2,:] - virtual_node[1,:]
     vector = orientation @ virtual
     cita[2] = -np.arctan2(vector[1],vector[2])
+    # if (cita[2] > np.pi/2) or (cita[2] < -np.pi/2):
+    #     cita[2] = 0.5*cita[3] 
     # vector = virtual + virtual * orientation[0,:]
     # cita[2] = np.arccos(vector @ orientation[2,:]/(np.linalg.norm(vector)*np.linalg.norm(orientation[2,:])))
     if cita[2]==0:
@@ -53,6 +49,8 @@ def forward_reaching_phase(target, orientation, cita, l, virtual_node, node):
     virtual = virtual_node[1,:] - virtual_node[0,:]
     vector = orientation @ virtual
     cita[1] = -np.arctan2(vector[0],vector[2])
+    # if (cita[1] > np.pi/2) or (cita[1] < -np.pi/2):
+    #     cita[1] = 0.5*cita[3] 
     # vector = virtual + virtual * orientation[1,:]
     # cita[1] = np.arccos(vector @ orientation[2,:]/(np.linalg.norm(vector)*np.linalg.norm(orientation[2,:])))
     if cita[1]==0:
@@ -67,20 +65,25 @@ def forward_reaching_phase(target, orientation, cita, l, virtual_node, node):
     node[1,:] = virtual_node[1,:] - l[1]*orientation[2,:]
 
     # Iteration 4
-    cita[0] = np.arccos(np.array([0, 0, 1]) @ orientation[2,:])/(np.linalg.norm(orientation[2,:]))
+    virtual = np.array([0, 0, 1])
+    vector = orientation @ virtual
+    cita[0] = -np.arctan2(vector[1],vector[2])
+    # if (cita[0] > np.pi/2) or (cita[0] < -np.pi/2):
+    #     cita[0] = 0.5*cita[3] 
     if cita[0]==0:
         l[0] = Sr/2
     else:
         l[0] = Sr/cita[0]*np.tan(cita[0]/2)
     virtual_node[0,:] = node[1,:] - l[0]*orientation[2,:]
     node[0,:] = virtual_node[0,:] - l[0]*np.array([0, 0, 1])
-    print(np.rad2deg(cita))
+
+    # print(np.rad2deg(cita,decimals=5))
 
     # Plot_3D(target, node,virtual_node)
     
     return cita
 
-def forward_kinematics (radians):
+def backward_reach (radians,Sr):
     M = [{'bend': np.eye(3), 'bend_inv': np.eye(3), 'displacement': np.zeros((3, 1)), 
         'B': np.eye(3)} for _ in range(5)]
     nodes = np.zeros((5,3))
@@ -120,6 +123,9 @@ def forward_kinematics (radians):
     for i in range(1, 5):
         nodes[i,:] = np.dot(M[i-1]['B'], M[i-1]['displacement']).T + nodes[i-1,:]
 
+    
+    virtual_node = np.zeros((4,3)) # virtual node initialization
+    l = np.array([0, 0, 0, 0]) # virtual link length initialization
     for i in range(4):
         if radians[i]==0:
             l[i] = Sr/2
@@ -130,32 +136,65 @@ def forward_kinematics (radians):
     orientation = M[4]['bend_inv']
     target = nodes[4,:]
     node = nodes[0:4,:]
+    # print(node)
+    # print(virtual_node)
+    # print(l)
 
-    return target, orientation, node, virtual_node,l
+    return target, orientation, node, virtual_node, l
 
-## MAIN ##
-# Generate the target through forward kinematics for test
-target_angle = np.array([0, 0, 0, 90])
-target_cita = np.deg2rad(target_angle)
-target, orientation, node, virtual_node, l  = forward_kinematics(target_cita)
-print(f"The target position of end effector: \n{target}\n")
-print(f"The orientation of end effector: \n{orientation}\n")
 
-# The initial position of manipulator
-cita = np.array([0,0,0,0], dtype=np.float64)
-l = np.array([Sr/2, Sr/2, Sr/2, Sr/2])
-virtual_node = np.array([[0, 0, 1*Sr/2], 
-                        [0, 0, 3*Sr/2], 
-                        [0, 0, 5*Sr/2], 
-                        [0, 0, 7*Sr/2]])
-node = np.array([[0, 0, 0*Sr], 
-                [0, 0, 1*Sr], 
-                [0, 0, 2*Sr], 
-                [0, 0, 3*Sr]])
+def FABRIKc(target, orientation,Sr,disp):
+    # The initial position of manipulator
+    cita = np.array([0,0,0,0], dtype=np.float64)
+    l = np.array([Sr/2, Sr/2, Sr/2, Sr/2])
+    virtual_node = np.array([[0, 0, 1*Sr/2], 
+                            [0, 0, 3*Sr/2], 
+                            [0, 0, 5*Sr/2], 
+                            [0, 0, 7*Sr/2]])
+    node = np.array([[0, 0, 0*Sr], 
+                    [0, 0, 1*Sr], 
+                    [0, 0, 2*Sr], 
+                    [0, 0, 3*Sr]])
 
-# LOOP
-for i in range(5):
-    cita = forward_reaching_phase(target, orientation, cita, l, virtual_node, node)
-    target_, orientation_, node, virtual_node, l = forward_kinematics(cita)
-    error = np.linalg.norm(target-target_)
-    print(error)
+    # LOOP
+    errors = []
+    citas = []
+    for i in range(200):
+        cita = forward_reach(target, orientation, cita, l, virtual_node, node,Sr)
+        cita_value = np.round(np.rad2deg(cita),decimals=2).tolist()
+        
+        citas.append(cita_value)
+        target_, _, _, _, l = backward_reach(cita,Sr)
+        error = np.round(np.linalg.norm(target-target_),decimals=5)
+        errors.append(error)
+        if disp == 0: # display the result
+            print(f"EPOCH {i+1}")
+            print(cita_value)
+            print(f"The angle of four segments are:\n {np.round(np.rad2deg(cita),decimals=2)}")
+            print(f"The error is {error}\n")
+
+        if errors.count(error) == 3:
+            print(f"WARMING: Error value repeated three times, adjust to avoid overfitting.")
+            cita = 0.1*cita
+            target_, _, _, _, l = backward_reach(cita,Sr)
+            citas[i] = cita
+            continue
+
+        # break
+        if error <= 0.05:
+            print(f"Error is less than or equal to 0.2. Exiting the loop at EPOCH {i+1}.")
+            break
+
+    if disp == 1: # not display the result
+        print(f"Convergence procesdure display: disp = 0")
+
+    index = np.argmin(errors)
+    cita = citas[index]
+    error = errors[index]
+
+    # Print the result in the form of table
+    t = PrettyTable(['Angles', cita])
+    t.add_row(['Error', error])
+    print(t)
+
+    return cita, error
